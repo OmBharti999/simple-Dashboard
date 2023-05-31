c208c62f42e1db95179ec4339544acd3956a8c55
enelint\NE14502580 <miraj@dataeximit.com>
2023-06-01 03:11:09 +0530

    fixed

--- web/frontend/components/graph/Graph.jsx
+++ web/frontend/components/graph/Graph.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { VictoryBar, VictoryChart } from "victory";
 
+const data = [
+  { quarter: 0, earnings: 0 },
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
+
+// <>graph</>
 function Graph() {
   return (
-    <div>Graph</div>
-  )
+    <section style={{ width: "400px", height: "300px" }}>
+      <VictoryChart>
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
+    </section>
+  );
 }
 
-export default Graph
+export { Graph };
