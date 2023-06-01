a6a6ad2574813fb392fb249cb524b8d719cf6fef
enelint\NE14502580 <miraj@dataeximit.com>
2023-06-01 22:41:47 +0530

    table prepared

--- web/frontend/components/table/Table.jsx
+++ web/frontend/components/table/Table.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from "react";
 
-function Table() {
+import "./index.css";
+
+function Table({ data }) {
+  const tableHeader = Object.keys(data[0]).filter((key) => key !== "id");
+  console.log("🚀 ~ file: Table.jsx:7 ~ Table ~ tableHeader:", tableHeader);
   return (
-    <section>
-        
-        <div>Table</div>
-    </section>
-    
-  )
+    <table className="table">
+      <thead>
+        <tr>
+          {tableHeader.map((field, index) => (
+            <th key={index}>{field}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((obj, index) => (
+          <tr key={obj.id}>
+            {tableHeader.map((field, index) => (
+              <td key={obj.id + String(index)}>
+                {field === "logo" ? (
+                  <img
+                    src={obj[field]}
+                    alt="airline Logo"
+                    width="100px"
+                    height="30px"
+                  />
+                ) : (
+                  obj[field]
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
--- web/frontend/components/table/tableFilter/Filter.jsx
+++ web/frontend/components/table/tableFilter/Filter.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import "./index.css";
 
 function Filter() {
   return (
-    <div>Filter</div>
-  )
+    <section className="filter ">
+      <select className="select date">
+        <option>current month</option>
+        <option>past 3 month</option>
+        <option>current year</option>
+      </select>
+
+      <select className="select price">
+        <option>below 1000</option>
+        <option>below 2000</option>
+        <option>below 5000</option>
+      </select>
+    </section>
+  );
 }
 
-export default Filter
+export default Filter;
